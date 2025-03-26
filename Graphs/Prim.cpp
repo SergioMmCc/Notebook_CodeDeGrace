5654da43fc8450afb67a68201dba1a53686dcf58
SergioMmCc <sergio.moncada1@utp.edu.co>
2025-03-25 21:32:53 -0500

    Update Prim and add CSES Graphs exercise Road Reparation

--- Graphs/Prim.cpp
+++ Graphs/Prim.cpp
@@ -1,41 +1,49 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-#define MAXN 200000
-
-const int inf = 2e9;
-
+using ll = long long;
+using ld = long double;
+#define pb push_back
+#define sz size()
+#define fi first
+#define se second
 typedef pair<int, int> pii;
-vector<pii> graph[MAXN+1];
+
+const ll inf = 1e15;
+const int maxn = 1e5 + 1;
+
+typedef pair<int, ll> pil;
+vector<vector<pil>> graph(maxn);
 
 // Comparador para ordenar la cola de prioridad por el peso menor
 struct comp{
-    bool operator() (pii a, pii b){
+    bool operator() (pil a, pil b){
         return a.second > b.second; // Para Maximal Spanning Tree cambiar '>' por '<'
     }
 };
 
 // Algoritmo de Prim para hallar el Minimal Spanning Tree de un grafo no dirigido y conexo
 // Complejidad O(m * lg(n))
-void prim(int source, int n, vector<int>& distance, vector<int>& parent){
+void prim(int source, int n, vector<ll>& d, vector<int>& parent){
     vector<bool> inQueue(n+1, 1);
     for(int i = 1; i <= n; i++){
-        distance[i] = inf;
+        d[i] = inf;
         parent[i] = -1;
     }   
-    distance[source] = 0;
-    priority_queue<pii, vector<pii>, comp> pq;
+    d[source] = 0;
+    priority_queue<pil, vector<pil>, comp> pq;
     pq.push({source, 0});
     while(!pq.empty()){
         int u = pq.top().first;
         pq.pop();
         if(!inQueue[u]) continue;
         inQueue[u] = 0;
-        for(pii v : graph[u]){
-            if(inQueue[v.first] && distance[v.first] > v.second){
-                distance[v.first] = v.second;
-                parent[v.first] = u;
-                pq.push({v.first, distance[v.first]});
+        for(pil edge : graph[u]){
+            int v = edge.fi; ll w = edge.se;
+            if(inQueue[v] && d[v] > w){
+                d[v] = w;
+                parent[v] = u;
+                pq.push({v, d[v]});
             }
         }
     }
@@ -43,20 +51,21 @@ void prim(int source, int n, vector<int>& distance, vector<int>& parent){
 
 int main() {
     ios_base::sync_with_stdio(0);cin.tie(NULL);
-    int n, m;cin>>n>>m;
+    int n, m; cin>>n>>m;
     for(int i = 0; i < m; i++){
-        int u, v, w;
+        int u, v; ll w;
         cin>>u>>v>>w;
         graph[u].push_back({v, w});
         graph[v].push_back({u, w});
     }
-    vector<int> distance(n+1), parent(n+1);
-    prim(1, n, distance, parent);
+    vector<ll> d(n+1);
+    vector<int> parent(n+1);
+    prim(1, n, d, parent);
     
     //Para calcular el peso total en el MST
-    long long totalWeight = 0;
+    ll totalWeight = 0;
     for(int i = 1; i <= n; i++)
-        totalWeight += distance[i];
+        totalWeight += d[i];
     cout<<totalWeight<<endl;
 
     return 0;
