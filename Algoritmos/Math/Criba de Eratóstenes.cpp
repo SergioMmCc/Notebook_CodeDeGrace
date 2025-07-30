e6cc32af8ca73882addb5f73264e7ec2b8a9ea97
SergioMmCc <sergio.moncada1@utp.edu.co>
2025-07-29 22:20:28 -0500

    Finish USACO Bitmask DP

--- Algoritmos/Math/Criba de Eratóstenes.cpp
+++ Algoritmos/Math/Criba de Eratóstenes.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 #define endl '\n'
 using ll = long long;
+#define pb push_back
 const int limite = 10001000; // Hasta este numero hay un poco mas de 6e5 primos
 
 vector<bool> esPrimo(limite + 1, 1);
+vector<int> primos;
 
 void sieve(){
     esPrimo[0] = esPrimo[1] = 0;
@@ -13,6 +15,9 @@ void sieve(){
             for(ll i = p * p; i <= limite; i += p) esPrimo[i] = 0;
         }
     }
+
+    for(int i = 2; i < limite; i++)
+        if(esPrimo[i]) primos.pb(i); 
 }
 
 int main() {
