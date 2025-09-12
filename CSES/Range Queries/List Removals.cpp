1aab6f27de2595fdcd04205533b73cc4d78261fb
SergioMmCc <sergio.moncada1@utp.edu.co>
2025-09-11 23:44:41 -0500

    Add Range Queries exercises

--- CSES/Range Queries/List Removals.cpp
+++ CSES/Range Queries/List Removals.cpp
@@ -10,11 +10,25 @@ using ld = long double;
 typedef pair<int, int> pii;
 #include<ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
+
 using ordered_set = 
-    tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
+    tree<pii, null_type, less<pii>, rb_tree_tag, tree_order_statistics_node_update>;
 
 void solver(){
-    
+    int n; cin>>n;
+    ordered_set nums;
+    for(int i = 0; i < n; i++){
+        int save; cin>>save;
+        nums.insert({i, save});
+    }
+
+    for(int i = 0; i < n; i++){
+        int del; cin>>del; del--;
+        auto it = nums.find_by_order(del);
+        if(i) cout<<' ';
+        cout<<(*it).se;
+        nums.erase(it);
+    }
 }
 
 int main(){
