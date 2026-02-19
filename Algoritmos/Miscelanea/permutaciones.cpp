eaed652e8ac05f3910d15e766fc196cc6efdc2ce
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-02-18 23:37:02 -0500

    Add Rounds 1076, 1079, Ed 185 and Hello 2026

---
+++ Algoritmos/Miscelanea/permutaciones.cpp
@@ -0,0 +1,5 @@
+// Entrego una permutacion y me devuelve la siguiente o la anterior.
+// Cuando ya no hay ams, devuelve false.
+// Complejidad por llamado O(n)
+next_permutation(all(v)); // Si quiero verlas todas, ordenar ascendente
+prev_permutation(all(v)); // Si quiero verlas todas, ordenar descendente
