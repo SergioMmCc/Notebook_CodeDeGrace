16109f541dcce59d13177eb37a74d0e8375623d8
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-02-26 10:10:01 -0500

    Add Ed Round 187

--- Algoritmos/Orden y busqueda en vectores/biseccion.cpp
+++ Algoritmos/Orden y busqueda en vectores/biseccion.cpp
@@ -1,13 +1,13 @@
 // Con avance r = m, l = m + 1 (Hallar el menor que cumple)
 while(l < r){
     int m = (l + r) / 2;
-    if(check(m)) r = m;
+    if(can(m)) r = m;
     else l = m + 1;
 }
 
 // Con avance r = m - 1, l = m (Hallar el mayor que cumple)
 while(l < r){
     int m = (l + r + 1) / 2;
-    if(check(m)) l = m;
+    if(can(m)) l = m;
     else r = m - 1;
 }
