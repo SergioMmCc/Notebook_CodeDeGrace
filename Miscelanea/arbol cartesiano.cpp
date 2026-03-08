49267b635fa7990bd8c1d12961e7e51717d74eab
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-03-07 23:20:22 -0500

    Add RPC 2026 01 and exercises CSES DP Mountain Range and Round 1078 E

--- Miscelanea/arbol cartesiano.cpp
+++ Miscelanea/arbol cartesiano.cpp
@@ -1,36 +1,37 @@
-// Teniendo un array a de numeros distintos, armar su arbol 
-// cartesiano, es decir, un arbol donde la raiz es el mayor 
-// elemento y para cada nodo, sus 2 hijos son menores. El hijo 
-// izquierdo es un nodo menor que se encuentra a la izquierda 
-// en el array y el hijo derecho es un nodo menor que se 
-// encuentra a la derecha en el array.
+// Teniendo un array a, armar su arbol cartesiano, es decir, un 
+// arbol donde la raiz es el mayor elemento y para cada nodo, sus 2 
+// hijos son menores. El hijo izquierdo es un nodo menor que se 
+// encuentra a la izquierda en el array y el hijo derecho es un nodo 
+// menor que se encuentra a la derecha en el array.
 // Proceso:
 // - Crear una pila
 // - Recorrer el array en orden i -> [1, n]
 // - Sacar de la pila todos los elementos menores a a[i]
-// - El ultimo elemento que se saca es el hijo izquierdo de a[i]
-// - Si la pila no esta vacia, a[i] es el hijo derecho del tope
-// - Se inserta a[i] en la pila
+// - El ultimo elemento que se saca es el hijo izquierdo de i
+// - Si la pila no esta vacia, i es el hijo derecho del tope
+// - Se inserta i en la pila
 // Complejidad: O(n)
 // Notas:
 // - Si en vez de arbol donde cada hijo es menor, se quiere uno
 //   donde cada hijo sea mayor, en lugar de sacar de la pila todos
 //   los elementos menores que a[i] en la pila, se sacan todos los
 //   mayores.
-// - Si los elementos de a son muy grandes, comprimir.
+// - En caso de que el array tenga numeros repetidos, la raiz del
+//   arbol sera el indice de la primera ocurrencia del mayor 
+//   elemento del array.
 
 void cartesianTree(int n, vi& a, vi& sl, vi& sr){
     stack<int> st;
-    for(int i = 1; i <= n; i++){
+    for(int i = 0; i < n; i++){
         int last = -1; 
-        while(!st.empty() && a[i] > st.top()){
+        while(!st.empty() && a[i] > a[st.top()]){
             last = st.top();
             st.pop();
         }
 
-        if(last != -1) sl[a[i]] = last;
-        if(!st.empty()) sr[st.top()] = a[i];
+        if(last != -1) sl[i] = last;
+        if(!st.empty()) sr[st.top()] = i;
 
-        st.push(a[i]);
+        st.push(i);
     }
 }
--- Miscelanea/permutaciones.cpp
+++ Miscelanea/permutaciones.cpp
@@ -1,5 +1,5 @@
 // Entrego una permutacion y me devuelve la siguiente o la anterior.
-// Cuando ya no hay ams, devuelve false.
+// Cuando ya no hay mas, devuelve false.
 // Complejidad por llamado O(n)
 next_permutation(all(v)); // Si quiero verlas todas, ordenar ascendente
 prev_permutation(all(v)); // Si quiero verlas todas, ordenar descendente
