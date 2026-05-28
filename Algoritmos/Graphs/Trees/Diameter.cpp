90efbd026ae91f2cfad98ed7102a4968ce640a93
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-05-27 20:37:57 -0500

    Add Exactly K Steps exercise to USACO Gold Trees Eulerian Tour and Diameter theory

---
+++ Algoritmos/Graphs/Trees/Diameter.cpp
@@ -0,0 +1,8 @@
+/*
+El diametro de un arbol es la mayor distancia entre cualquier 
+par de nodos en el arbol.
+Si se tiene que el diametro de un arbol es la distancia entre
+dos vertices u, v, entonces podemos asumir que la mayor 
+distancia para un nodo x hacia cualquier nodo del arbol es 
+d(x, u) o d(x, v).
+*/
