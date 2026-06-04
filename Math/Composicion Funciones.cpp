60ca4585358d4a2286de3e40987f83d336c99bdf
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-06-03 21:16:59 -0500

    Add exercises 01 and Point Set Range Composite to USACO Platinum More Applications of SegTree and proporties of Composite functions

---
+++ Math/Composicion Funciones.cpp
@@ -0,0 +1,21 @@
+/*
+- f o g representa f(g(x))
+- No es conmutativa: f o g != g o f
+- Es asociativa: f o (g o h) = (f o g) o h
+- Funcion identidad: I(x) = x
+- Elemento neutro: f o I = I o f = f
+- Funcion inversa: ~f es la inversa de f, se halla 
+  despejando x y swapeando x con y.
+  Para poder hallarla se requiere que la funcion sea 
+  inyectiva (a cada elemento del dominio le
+  corresponde un elemento diferente del codominio, 
+  es decir, si f(a) = f(b) entonces obligatoriamente
+  a = b), de lo contrario se debe restringir el
+  dominio para que se cumpla esa propiedad.
+- Componer con la inversa: f o ~f = ~f o f = x
+- La inversa de la composicion de 2 funciones es 
+  igual a la composicion de sus inversas en orden 
+  contrario: ~(f o g) = ~g o ~f
+- Para realizar la composicion NO es necesario saber 
+  de antemano el valor de x
+*/
