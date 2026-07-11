ccf2d815ef8db142abc06329c909a192c240cf5a
Felipe <j.f.sanchez1011@gmail.com>
2026-07-11 14:16:23 -0500

    Primos correcto lugar

--- Algoritmos/Math/Number Theory/Generar_primos.cpp
+++ Algoritmos/Math/Number Theory/Generar_primos.cpp
@@ -1,23 +1,22 @@
 // Encuentra todos los numeros primos entre 1 y n
 // Complejidad: O(n * log(log(n)))
+// Algoritmo: Criba de Eratóstenes.
 
 // Se usa limite como n
 // Hasta este numero hay un poco mas de 6e5 primos
+// Fomula de cantidad de primos aproximado dado n -> n / log(n)
 const int limite = 10001000;
 
-vb esPrimo(limite + 1, 1);
-vi primos;
-
+vector<bool> esPrimo(limite + 1, 1);
+vector<int> primos;
 
 void sieve(){
     esPrimo[0] = esPrimo[1] = 0;
-    for(ll p = 2; p * p <= limite; p++){
+    for(ll p = 2; p <= limite; p++){
         if(esPrimo[p]){
+            primos.push_back(p);
             for(ll i = p * p; i <= limite; i += p) esPrimo[i] = 0;
         }
     }
-
-    for(int i = 2; i < limite; i++)
-        if(esPrimo[i]) primos.pb(i); 
 }
 
