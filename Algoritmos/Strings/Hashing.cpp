2da12867115145b68109be771f783e71f99c7446
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-07-15 12:50:13 -0500

    Update Hashing and IndexedSet

--- Algoritmos/Strings/Hashing.cpp
+++ Algoritmos/Strings/Hashing.cpp
@@ -1,8 +1,6 @@
 // Algunas notas:
-// - Usar Hashing en codeforces puede ser peligroso debido al hacking
 // - La probabilidad de que 2 cadenas tengan el mismo hashing es de n / m
 //   siendo n la longitud de la cadena mas larga
-// - Lo ideal es utilizar un p de al menos la longitud de la cadena mas larga
 // - Si tengo un conjunto de hashes de k cadenas con longitud de maximo n,
 //   la probabilidad de que 2 cadenas tengan igual hash es de (k^2 * n) / m
 //   Por ende, si k y n son de alrededor de 1e5 y m alrededor de 1e9, es 
@@ -11,49 +9,31 @@
 //   enteros de 128 bits para las multiplicaciones, aunque ahora el hashing 
 //   y las queries serian un poco mas lentas. (Para hacerlo puedo simplemente
 //   poner using ll = __int128; y cambiar el modulo).
-// - Otra estrategia puede ser utilizar doble hashing, es decir, darle 2 claves
-//   a cada palabra, lo cual reduce considerablemente la probabilidad de colision.
+// - Al usar doble modulo se estan reduciendo esas probabilidades considerablemente
 
-// Halla el hashing de una cadena en O(n)
-ll compute_hash(string s){
-    ll p = 31; // Si s se compone de lowercase english letters
-    // ll p = 53; // Si s se compone de lowercase y uppercase english letters
-    ll p_pow = 1; // Si precalculo las potencias de p podria ser ligeramente mas eficiente
-    ll m = 1e9 + 9;
-    ll ans = 0;
+// Hashing con doble modulo
+// En caso de necesitar otro modulo
+// 1000234999, 1000567999, 1000111997, 1000777121, 999727999, 1070777777
+const int MOD[] = { 1001864327, 1001265673 }, N = 1e6 + 10;
+const pii BASE(257, 367), ZERO(0, 0), ONE(1, 1);
+inline int add(int a, int b, int mod) { return a+b >= mod ? a+b-mod : a+b; }
+inline int sbt(int a, int b, int mod) { return a-b < 0 ? a-b+mod : a-b; }
+inline int mul(int a, int b, int mod) { return ll(a) * b % mod;} 
+inline ll operator ! (const pii a) { return (ll(a.fi) << 32) | a.se; }
+inline pii operator + (const pii& a, const pii& b) {
+  return {add(a.fi, b.fi, MOD[0]), add(a.se, b.se, MOD[1])};}
+inline pii operator - (const pii& a, const pii& b) {
+  return {sbt(a.fi, b.fi, MOD[0]), sbt(a.se, b.se, MOD[1])};}
+inline pii operator * (const pii& a, const pii& b) {
+  return {mul(a.fi, b.fi, MOD[0]), mul(a.se, b.se, MOD[1])};}
 
-    for(char c : s){
-        ans = (ans + (c - 'a' + 1) * p_pow) % m;
-        p_pow = (p_pow * p) % m; 
-    }
-
-    return ans;
-}
-
-// Sirve para hacer precalculos para hacer queries de la subcadena
-// sl, sl+1, ..., sr
-// Construir el array toma O(n), las queries son O(1)
-void precalc_hashing(string s, vector<ll>& h, vector<ll>& ph){
-    ll p = 31;
-    ll m = 1e9 + 9;
-
-    h[0] = s[0] - 'a' + 1;
-    ph[0] = 1;
-
-    for(int i = 1; i < s.sz; i++){
-        h[i] = (((h[i-1] * p) % m) + (s[i] - 'a' + 1)) % m;
-        ph[i] = (ph[i-1] * p) % m;
-    }
-
-    /* 
-        Para hacer queries:
-        - Si l = 0: h[r]
-        - Si no: (h[r] - h[l-1] * ph[r-l+1]) % m
-    */
-}
-
-ll queries(ll l, ll r, vector<ll>& h, vector<ll>& ph){
-    ll m = 1000000009;
-    if(!l) return h[r];
-    return (h[r] - ((h[l-1] * ph[r-l+1]) % m) + m) % m;
-}
+pii base[N]{ONE};
+void prepare() { for1(i, N-1) base[i] = base[i-1] * BASE; }
+template <class type>
+struct hashing {   /// HACELEEE PREPAREEEE en el main!!!
+  vii ha;       // ha[i] = t[i]*p0 + t[i+1]*p1 + t[i+2]*p2 + ..
+  hashing(type &t): ha(sz(t)+1, ZERO){ // O(n)
+    for(int i = sz(t) - 1; i >= 0; --i) ha[i] = ha[i+1] * BASE + pii{t[i], t[i]};
+  }
+  pii query(int l, int r){ return ha[l] - ha[r+1] * base[r-l+1]; } // [l,r], O(1)
+};
