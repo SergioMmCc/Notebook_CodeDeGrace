d8ad16fbd3412f1b5b15abb622cffc608a83371a
Felipe <j.f.sanchez1011@gmail.com>
2026-07-15 17:17:28 -0500

    Add constant to template

--- Algoritmos/template.cpp
+++ Algoritmos/template.cpp
@@ -24,6 +24,9 @@ typedef vector<pll> vll;
 // using namespace __gnu_pbds;
 // using indexed_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
 
+const ll INFL=1e18;
+const int INF=1e9, MOD=1e9+7;
+
 void solver(){
     
 }
