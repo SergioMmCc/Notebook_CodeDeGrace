de72c8dd8bb96ad190f248427f98b660562ce7a2
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-07-20 22:58:51 -0500

    Update Kadane and Rectangular Partial Sums

--- Range Queries/Rectangular Partial Sums.cpp
+++ Range Queries/Rectangular Partial Sums.cpp
@@ -1,11 +1,4 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define endl '\n'
-using ll = long long;
-using ld = long double;
-#define pb push_back
-#define sz size()
-typedef pair<int, int> pii;
+#include "../template.h"
 
 const int maxn = 1001;
 int a[maxn][maxn];
@@ -16,8 +9,8 @@ void solver(){
     
     // La formula para calcular cada termino es:
     // S[i][j] = A[i][j] + S[i−1][j] + S[i][j−1] − S[i−1][j−1]
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
+    for1(i,n){
+        for1(j,m){
             cin>>a[i][j];
             s[i][j] = (ll)a[i][j] + s[i-1][j] + s[i][j-1] - s[i-1][j-1];
         }
@@ -32,15 +25,4 @@ void solver(){
         int x1, y1, x2, y2; cin>>x1>>y1>>x2>>y2;
         cout<<s[y2][x2] - s[y1-1][x2] - s[y2][x1-1] + s[y1-1][x1-1]<<endl;
     }
-}
-
-int main(){
-    ios_base::sync_with_stdio(0);cin.tie(NULL);
-    int t = 1;
-    // cin>>t;
-    while(t--){
-        solver();
-    }
-
-    return 0;
 }
